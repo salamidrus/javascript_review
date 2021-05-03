7a17943d2474155a336f98121ff3f3550916c510
idrus <idrussalam95@gmail.com>
2021-05-03 17:24:08 +0700

    Add Closure Playground

---
+++ js_interview/closuresPlayground.js
@@ -0,0 +1,51 @@
+function init() {
+  let name = "Darwin";
+  function showName() {
+    console.log(name);
+  }
+  showName();
+}
+
+// init();
+
+function initRef() {
+  return function (name) {
+    console.log(name);
+  };
+}
+
+let initExec = initRef();
+// initExec("Karina");
+
+// function factories
+function greeting(time) {
+  return function (name) {
+    console.log(`Halo ${name}, Good ${time}`);
+  };
+}
+
+let morningGreeting = greeting("morning");
+let afternoonGreeting = greeting("afternoon");
+// morningGreeting("Darwin");
+// morningGreeting("Karina");
+// afternoonGreeting("Darwin");
+
+// immediate executed function
+let add = (function () {
+  let counter = 0;
+  return function () {
+    return ++counter;
+  };
+})();
+
+counter = 100;
+
+console.log(add());
+console.log(add());
+console.log(add());
+
+// let count = add();
+// console.log(count());
+// console.log(count());
+// console.log(count());
+// console.log(count());
